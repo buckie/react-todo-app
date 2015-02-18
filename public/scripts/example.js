2bd25bb1ec506402c49ebb85933c9d4758d32e79
erindepew <erinldepew@gmail.com>
2015-02-18 15:14:28 -0500

    splitting out hours and instances classes

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,4 +1,5 @@
 
+ //creating the to-do list from the create new item
 
 var ToDoList = React.createClass({
     loadListFromServer: function() {
@@ -51,6 +52,128 @@ var ToDoList = React.createClass({
 });
 
 
+
+ // creating a new item header section
+
+ var CreateNewItem= React.createClass({
+     handleSubmit: function(e) {
+         e.preventDefault();
+         var label = this.refs.label.getDOMNode().value.trim();
+         var number = this.refs.number.getDOMNode().value.trim();
+         var hours = this.refs.hours.getDOMNode().checked;
+         var instances = this.refs.instances.getDOMNode().checked;
+
+         console.log("create new item variables: " + hours);
+         if (!label) {
+             return;
+         }
+         this.props.onItemSubmit({label:label, number:number, hours:hours, instances:instances});
+         this.refs.label.getDOMNode().value = '';
+         this.refs.number.getDOMNode().value = '';
+         this.refs.hours.getDOMNode().checked = '';
+         this.refs.instances.getDOMNode().checked = '';
+         return;
+     },
+     render: function() {
+         return (
+             <form className="listForm" onSubmit={this.handleSubmit}>
+                 <input type="text" placeholder="enter a label" ref="label"/>
+                 <input type="number" placeholder="enter a number" min="1" max="7" ref="number" />
+                 <input type="radio" name="type" value="hours" ref="hours" />number of hours
+                 <input type="radio" name="type" value="instances" ref="instances"/>number of instances
+                 <input type="submit" value="Post" />
+             </form>
+             );
+     }
+ });
+
+ //creating an instances item
+
+
+ var ItemInstance = React.createClass({
+     handleCheck: function(e) {
+         var amount = 50/this.props.number;
+         $('.progress-bar').css(
+             'width', "+=" + amount);
+         return amount;
+     },
+     createItemInstance: function(){
+         var checkboxes = [];
+         for (var i=0; i < this.props.number; i++) {
+             checkboxes.push(<input type="checkbox" onClick={this.handleCheck}/>);
+             console.log("adding to inputs");
+         }
+         this.props.checkboxes = checkboxes;
+     },
+     render: function() {
+         this.createItemInstance();
+         return (
+             <form className="checkboxes-list">
+             {this.props.checkboxes}
+                 </form>
+             );
+         }
+
+             });
+
+ // creating hours item
+
+ var ItemHours = React.createClass({
+     handleSubmit: function(e) {
+         var amount = 50/this.props.number;
+         $('.progress-bar').css(
+             'width', "+=" + amount);
+         return amount;
+     },
+     render: function() {
+         return (
+             <form className="submit-item">
+                 <input type="text" name="type" value="hours" ref="hours" placeholder="enter number of hours" min="1" max="10" ref="number"/>
+                 <input type="submit" value="Post" />
+             </form>
+             );
+     }
+
+ });
+
+
+
+ //creating an item logic: instances vs. hours
+
+ var Item = React.createClass({
+
+     createInputs: function() {
+         var inputs = [];
+         if (this.props.instances == true){
+             inputs.push(<ItemInstance  number={this.props.number}/>);
+         }
+         else if (this.props.hours == true){
+             inputs.push(<ItemHours  number={this.props.number}/>);
+         }
+
+         else{
+             alert("please make a selection");
+         }
+
+         this.props.inputs = inputs;
+     },
+     render: function() {
+         this.createInputs();
+         return (
+             <div className="toDoItem">
+                 <h2 className="label">
+                {this.props.label}
+                 </h2>
+            {this.props.inputs}
+                 <div className="progress-bar"></div>
+             </div>
+             );
+     }
+ });
+
+//create a list of items to insert into the page
+
+
 var ToDoItems = React.createClass({
     render: function() {
         var listNodes = this.props.data.map(function (data, index) {
@@ -66,76 +189,7 @@ var ToDoItems = React.createClass({
     }
 });
 
-var Item = React.createClass({
-    handleCheck: function(e) {
-        var amount = 50/this.props.number;
-        $('.progress-bar').css(
-            'width', "+=" + amount);
-    },
-    createInputs: function() {
-    var inputs = [];
-    console.log("checked element:" + this.props.hours);
-        if (this.props.instances == true){
-            for (var i=0; i < this.props.number; i++) {
-                inputs.push(<input type="checkbox" onClick={this.handleCheck}/>);
-                console.log("adding to inputs");
-            };
-        }
-        else if (this.props.hours == true){
-            inputs.push(<input type="text" placeholder="enter number of hours" min="1" max="10" ref="number"/>);
-        }
 
-        else{
-            alert("please make a selection");
-        }
-
-    this.props.inputs = inputs;
-    },
-    render: function() {
-        this.createInputs();
-        return (
-            <div className="toDoItem">
-                <h2 className="label">
-                {this.props.label}
-                </h2>
-            {this.props.inputs}
-                <div className="progress-bar"></div>
-            </div>
-            );
-    }
-});
-
-var CreateNewItem= React.createClass({
-    handleSubmit: function(e) {
-        e.preventDefault();
-        var label = this.refs.label.getDOMNode().value.trim();
-        var number = this.refs.number.getDOMNode().value.trim();
-        var hours = this.refs.hours.getDOMNode().checked;
-        var instances = this.refs.instances.getDOMNode().checked;
-
-        console.log("create new item variables: " + hours);
-        if (!label) {
-            return;
-        }
-        this.props.onItemSubmit({label:label, number:number, hours:hours, instances:instances});
-        this.refs.label.getDOMNode().value = '';
-        this.refs.number.getDOMNode().value = '';
-        this.refs.hours.getDOMNode().checked = '';
-        this.refs.instances.getDOMNode().checked = '';
-        return;
-    },
-    render: function() {
-        return (
-            <form className="listForm" onSubmit={this.handleSubmit}>
-                <input type="text" placeholder="enter a label" ref="label"/>
-                <input type="number" placeholder="enter a number" min="1" max="7" ref="number" />
-                <input type="radio" name="type" value="hours" ref="hours" />number of hours
-                <input type="radio" name="type" value="instances" ref="instances"/>number of instances
-                <input type="submit" value="Post" />
-            </form>
-            );
-    }
-});
 
 
 
